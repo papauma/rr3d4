956c92139387aafa41fd50bbe2d05b54baf4f429
salvador.escriva@inetum.com <salvador.escriva@inetum.com>
2023-07-30 18:03:54 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
+import ContextualComponents from '@src/components/widgets/ContextualComponents';
+import { LanguageProvider } from '@src/context/languageContext';
+import { store } from '@src/redux/store';
+import Precarga from '@src/screens/Precarga';
 import Routes from '@src/screens/Router';
+import Splash from '@src/screens/splash/components/splash/Splash';
 import React, { useState } from 'react';
-import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
-import { store } from '@src/redux/store';
-import { LanguageProvider } from '@src/context/languageContext';
-import Precarga from '@src/screens/Precarga';
-import { ThemeProvider } from '@src/context/themeContext';
-import Splash from '@src/screens/splash/components/splash/Splash';
-import LocationTracker from '@src/components/widgets/LocationTracker';
-import ContextualComponents from '@src/components/widgets/ContextualComponents';
 
 export default function App() {
   const [precargaLoaded, setPrecargaLoaded] = useState(false);
@@ -21,26 +18,20 @@ export default function App() {
   };
   return (
     <GestureHandlerRootView style={ {flex: 1} }>
-      <ThemeProvider>
         <LanguageProvider>
             <Provider store={store}>
               <Precarga onFinish={finishPrecarga}>
-                <LocationTracker>
-                  <ContextualComponents>
-                    <StatusBar barStyle={'dark-content'} />
                     {!precargaLoaded ? (
                       <Splash/>
-                    ) : (
-                      <>
+                      ) : (
+                        <>
                         <Routes />
                       </>
                     )}
-                  </ContextualComponents>
-                </LocationTracker>
               </Precarga>
+              <ContextualComponents />
             </Provider>
           </LanguageProvider>
-        </ThemeProvider>
     </GestureHandlerRootView>
   );
 }
