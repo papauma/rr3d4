5f085655fae09b35d6e9330e87124bbf63fee38d
salvador.escriva@inetum.com <salvador.escriva@inetum.com>
2023-07-30 18:36:39 +0200

    manifest i literals

--- src/resources/i18/ca.ts
+++ src/resources/i18/ca.ts
@@ -5,8 +5,11 @@
 export const strings = {
   ajudans_main: "Ajuda'ns a millorar",
   descripcio_main: 'Amb aquesta aplicació ens podràs reportar totes aquelles coses del poble què consideres que es poden millorar.',
+  canviIdioma_main: "Vols canviar l'idioma de l'aplicació?",
   titol_reportText: 'Descriu el que ens vols comunicar',
   info1_reportText: 'Descriu de manera breu i clara allò que ens vulgues reportar.',
+  canviIdiomCas_main: 'Castellà',
+  canviIdiomaVal_main: 'Valencià',
 
   afegeix_reportPhoto: 'Afegeix una imatge',
   utilitzar_reportPhoto: 'Utilitzar aquesta imatge',
