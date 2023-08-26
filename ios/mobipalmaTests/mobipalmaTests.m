3de885cafca2aaf8ad428d4da4ec3b599537170a
salvador.escriva@inetum.com <salvador.escriva@inetum.com>
2023-08-26 15:04:17 +0200

    canvis ios

--- ios/mobipalmaTests/mobipalmaTests.m
+++ ios/mobipalmaTests/mobipalmaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobipalmaTests : XCTestCase
+@interface benirredraTests : XCTestCase
 
 @end
 
-@implementation mobipalmaTests
+@implementation benirredraTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
